4fdba4b10fb9495218db9fb2342be37d2d975b04
davetayls <dave.taylor@pogokid.com>
2020-12-02 17:32:28 +0000

    added simple cache functionality

---
+++ functions/test-cache.js
@@ -0,0 +1,25 @@
+exports.handler = async (event, context, callback) => {
+  // Get the request URL
+  const { path } = event;
+  console.log("[request]", path);
+  // path as:
+  // /test/max-0/smax-3000
+  // /test/max-/smax-0
+  // /test/max-3000/smax-
+  const [_, max, smax] = /^\/test\/max-(\d*)\/smax-(\d*)/.exec(path)
+
+  const control = [
+    'public',
+    max ? `max-age=${max}` : null,
+    smax ? `s-maxage=${smax}` : null,
+    'must-revalidate'
+  ].filter(Boolean).join(', ')
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Cache-Control': control
+    },
+    body: `<html><title>test</title><body><h1>${control}</h1></body></html>`
+  }
+};
--- pages/index.js
+++ pages/index.js
@@ -6,60 +6,20 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Version 01
         </h1>
+        <h2>Construct a URL like</h2>
+        <p><code>/test/max-0/smax-3000/foo</code></p>
+        <p><code>/test/max-0/smax-3000/bar</code></p>
+        <p><code>/test/max-0/smax-/bar</code></p>
+        <p><code>/test/max-/smax-3000/bar</code></p>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
